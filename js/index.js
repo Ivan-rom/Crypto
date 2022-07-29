e69bb8babd7c2d2e1e853af27c8bc9fbafd48a1c
Ivan-rom <94216613+Ivan-rom@users.noreply.github.com>
2022-07-29 21:41:27 +0600

    Uploading final changes
    Final window with accordion

--- js/index.js
+++ js/index.js
@@ -2,3 +2,7 @@ import './module/menu.js';
 import './module/modal.js';
 import './module/tab-sabout-us.js';
 import './module/exchange-rates.js';
+// import './module/faq.js';
+import { accordion } from "./module/faq.js";
+
+accordion();
